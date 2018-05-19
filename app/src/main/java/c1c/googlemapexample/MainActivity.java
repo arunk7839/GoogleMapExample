dac4c0cd19ffae6eebad2b5ac9ed8c5855585bc5
Arun Chandravanshi <arunk7839@gmail.com>
2018-05-19 17:24:23 +0530

    Initial commit

---
+++ app/src/main/java/c1c/googlemapexample/MainActivity.java
@@ -0,0 +1,13 @@
+package c1c.googlemapexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
